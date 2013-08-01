cb9f73ff54ee83137ee766bde9888e347f3e3d6b
acidrave <breeze.fbn@gmail.com>
2013-08-01 11:51:34 +0300

    Delete run.bat

--- run.bat
+++
@@ -1,7 +0,0 @@
-"C:\Program Files (x86)\VMware\VMware Virtual Disk Development Kit\bin\vmware-mount.exe" z: C:\Emu\zx\unreal.ts\wc.hdd
-
-xcopy /y /s /e /c /h C:\Emu\zx\projects\cli.git\bin z:\
-
-"C:\Program Files (x86)\VMware\VMware Virtual Disk Development Kit\bin\vmware-mount.exe" /d z:
-
-C:\Emu\zx\unreal.ts\Unreal.exe
